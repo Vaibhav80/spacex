10c65dd6f6ea3db943adc9799d28185c72901fcb
Vaibhav80 <dhybhav@gmail.com>
2020-08-30 22:21:57 +0530

    handle api errors

--- pages/api/apis.js
+++ pages/api/apis.js
@@ -3,7 +3,18 @@ const baseUrl = "https://api.spaceXdata.com/v3/launches?limit=100";
 const fetchPrograms = async (data) => {
   const url = baseUrl.concat(buildQueryString(data));
 
-  return await fetch(url).then((res) => res.json());
+  return await fetch(url)
+    .then(handleErrors)
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const handleErrors = (response) => {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response.json();
 };
 
 const buildQueryString = (data) => {
